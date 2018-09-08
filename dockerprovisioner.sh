64b9bf8562224de4bf77f8d416c5ccbd44408018
Hemanth B <hemanth22hemu@gmail.com>
2018-09-08 11:40:45 +0530

    Update dockerprovisioner.sh

--- dockerprovisioner.sh
+++ dockerprovisioner.sh
@@ -1,2 +1,1 @@
-docker exec -it -u root $(docker ps -aq) /bin/bash -c "apt-get update"
-docker exec -it -u root $(docker ps -aq) /bin/bash -c "apt-get  install git curl binutils make gcc cpp g++ -y"
+apt-get update;apt-get  install git curl binutils make gcc cpp g++ -y
